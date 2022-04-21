3aead5c7fdee00ea8e3cf5b7861ba88fdf1c4ee0
Roman De Rose <romandr37@gmail.com>
2022-04-21 01:26:02 -0300

    seccion contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,54 @@
 import React from "react";
-
-// AGREGAR FRAMER MOTION
+import { motion } from "framer-motion";
+import "./Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact">
+      <form className="contact-form" action="">
+        <motion.input
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.4, type: "spring" }}
+          className="contact-input"
+          type="text"
+          placeholder="Nombre"
+          required
+        />
+        <motion.input
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.4, type: "spring", delay: 0.2 }}
+          className="contact-input"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <motion.textarea
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.4, type: "spring", delay: 0.4 }}
+          className="contact-input textarea"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Tu consulta"
+          required
+        ></motion.textarea>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.4, type: "spring", delay: 0.7 }}
+        >
+          <button className="contact-button" type="submit">
+            Enviar
+          </button>
+          <button type="reset">Borrar</button>
+        </motion.div>
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
